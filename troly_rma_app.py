7228a61a212bd68706a75068d3521e570861a04b
lhphuc3009 <lhphucgame3009@gmail.com>
2025-06-17 19:48:00 +0700

    troly_rma_app.py

--- troly_rma_app.py
+++ troly_rma_app.py
@@ -6,7 +6,7 @@ from yaml.loader import SafeLoader
 
 with open("auth_config.yaml") as file:
     config = yaml.load(file, Loader=SafeLoader)
-
+    st.write("DEBUG: config loaded =", config)
 authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
